bc53143d59c83964983814bb0b896ab1896c916d
Ben Wiswell <benjwiswell@gmail.com>
2021-08-11 03:30:31 -0400

    Revert "feat: initial perceptron struct and methods"
    This reverts commit 56fd4f41cd9fe29be30d4c25e84500e24bce43f3.

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/bwiswell/simple-go-network@master
-
-go 1.16
--- util/util.go
+++
@@ -1,7 +0,0 @@
-package util
-
-import "math"
-
-func Sigmoid(activity float64) float64 {
-	return 1 / (1 + math.Pow(math.E, -activity))
-}
